d861dff8b0baea497c1c558fcf0959ee321ed4b2
Arne Schwarck <arneschwarck@gmail.com>
2019-08-19 21:08:54 +0200

    for speed limits below 21 kph logic
    Enable acceleration for low speed limits. 
    
    If you are traveling in a high speed zone and you are not on the linear deceleration curve or at least 3 m/s close to it then you should accelerate until you have reached within 3 m/s of the constant decleration curve and then follow that till the low speed limit is reached.

--- selfdrive/controls/lib/planner.py
+++ selfdrive/controls/lib/planner.py
@@ -199,7 +199,7 @@ class Planner(object):
         #if speed_limit is not None:
         #  if v_ego + 20/3.6 > live_map_data.liveMapData.speedLimitAhead + (speed_limit - live_map_data.liveMapData.speedLimitAhead)*(live_map_data.liveMapData.speedLimitAheadDistance)/(speed_ahead_distance):
         #    distanceatlowlimit = 100
-        if speed_limit is not None and live_map_data.liveMapData.speedLimitAheadDistance > distanceatlowlimit and False:
+        if speed_limit is not None and live_map_data.liveMapData.speedLimitAheadDistance > distanceatlowlimit and v_ego + 3 < live_map_data.liveMapData.speedLimitAhead + (speed_limit - live_map_data.liveMapData.speedLimitAhead)*live_map_data.liveMapData.speedLimitAheadDistance/speed_ahead_distance:
           speed_limit_ahead = live_map_data.liveMapData.speedLimitAhead + (speed_limit - live_map_data.liveMapData.speedLimitAhead)*(live_map_data.liveMapData.speedLimitAheadDistance - distanceatlowlimit)/(speed_ahead_distance - distanceatlowlimit)
         else:
           speed_limit_ahead = live_map_data.liveMapData.speedLimitAhead
