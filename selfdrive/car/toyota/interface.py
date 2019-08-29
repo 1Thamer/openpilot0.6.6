7719869105954bbb838660901520fc863a103c04
Arne Schwarck <arneschwarck@gmail.com>
2019-08-29 08:06:28 +0200

    Lower ki and increase kp

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -94,8 +94,8 @@ class CarInterface(object):
     else:
       ret.gasMaxBP = [0.]
       ret.gasMaxV = [0.5]
-      ret.longitudinalTuning.kpV = [0.25, 0.25, 0.25]  # braking tune from rav4h
-      ret.longitudinalTuning.kiV = [0.020, 0.010]
+      ret.longitudinalTuning.kpV = [0.325, 0.325, 0.325]  # braking tune from rav4h
+      ret.longitudinalTuning.kiV = [0.001, 0.0010]
 
     ret.steerActuatorDelay = 0.12  # Default delay, Prius has larger delay
     if candidate != CAR.PRIUS:
